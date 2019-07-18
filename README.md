601c9e65c96935023638f1fe54d945519891d8ca
qhh0205 <qhh0205@gmail.com>
2019-07-18 21:32:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+### deploy-kit
+基于 Golang 的容器应用部署命令行工具，支持前端、后端部署。该命令行应用基于 golang 第三方 cli 包构建。
+
+### Usage
+部署两类服务：app（微服务），web（前端服务），部署微服务指定 app 命令，部署前端服务指定 web 命令。
+```
+NAME:
+   deploy - deploy application
+
+USAGE:
+   deploy [global options] command [command options] [arguments...]
+
+VERSION:
+   v1.0
+
+COMMANDS:
+     list, ls           list all of services
+     app                deploy microservice application
+     web                deploy web application
+     lsbranch, lsb      list the code branches of service
+     upload-cdn, upcdn  upload file or directory to gcs bucket
+     help, h            Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --help, -h     show help
+   --version, -v  print the version
+```
+
+#### 使用示例
+列出当前有哪些服务可以部署：
+```bash
+deploy ls
+```
+列出 user 服务代码有哪些分支：
+```bash
+deploy lsb -s user
+```
+部署 user 微服务 master 分支到 stage 环境：
+```bash
+deploy app -s user -b master -e stage
+```
+
+部署前端 admin 服务 master 分支到 Stage 环境
+```bash
+deploy web -s admin -b master -e stage
+```
+
+### 相关配置
+需要在 `$HOME/.dpcfg/` 目录下放置相关配置文件：
+- conf.yaml：部署配置文件；
+- service.yaml：服务列表信息；
+- MicroServiceDockerfile: 微服务 Dockerfile 文件；
+- kube-confg: Kubernetes 集群连接配置文件；
+- _json_key: gcr 镜像仓库认证文件;
+### 工具依赖包
+```
+go get github.com/olekukonko/tablewriter
+go get github.com/urfave/cli
+go get gopkg.in/yaml.v3
+```
+
---
+++ main.go
@@ -0,0 +1,375 @@
+package main
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/urfave/cli"
+	"gopkg.in/yaml.v3"
+)
+
+var version = "v1.0"
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "deploy"
+	app.Usage = "deploy application"
+	app.Version = version
+	app.Commands = []cli.Command{
+		{
+			Name:    "list",
+			Aliases: []string{"ls"},
+			Usage:   "list all of services",
+			Action: func(c *cli.Context) (err error) {
+				return lsService()
+			},
+		},
+		{
+			Name:  "app",
+			Usage: "deploy microservice application",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "service, s",
+					Value: "",
+					Usage: "service to deploy",
+				},
+				cli.StringFlag{
+					Name:  "branch, b",
+					Value: "",
+					Usage: "code branch to deploy",
+				},
+				cli.StringFlag{
+					Name:  "env, e",
+					Value: "",
+					Usage: "environment to deploy, available env is: [dev, test, stage, production]",
+				},
+			},
+			Action: func(c *cli.Context) (err error) {
+				return deployMicroService(c.String("service"), c.String("branch"), c.String("env"))
+			},
+		},
+		{
+			Name:  "web",
+			Usage: "deploy web application",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "service, s",
+					Value: "",
+					Usage: "service to deploy",
+				},
+				cli.StringFlag{
+					Name:  "branch, b",
+					Value: "",
+					Usage: "code branch to deploy",
+				},
+				cli.StringFlag{
+					Name:  "env, e",
+					Value: "",
+					Usage: "environment to deploy, available env is: [dev, test, stage, production]",
+				},
+			},
+			Action: func(c *cli.Context) (err error) {
+				fmt.Println("ToDO......")
+				return nil
+			},
+		},
+		{
+			Name:    "lsbranch",
+			Aliases: []string{"lsb"},
+			Usage:   "list the code branches of service",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "service, s",
+					Value: "",
+					Usage: "service to list branches",
+				},
+			},
+			Action: func(c *cli.Context) (err error) {
+				return lsBranch(c.String("service"))
+			},
+		},
+		{
+			Name:      "upload-cdn",
+			Aliases:   []string{"upcdn"},
+			Usage:     "upload file or directory to gcs bucket",
+			ArgsUsage: "<src> <dst>",
+			Flags: []cli.Flag{
+				cli.BoolTFlag{
+					Name:  "cache, c",
+					Usage: "the file or directory in gcs bucket cache or not",
+				},
+			},
+			Action: func(c *cli.Context) (err error) {
+				if len(c.Args()) < 2 {
+					cli.ShowCommandHelp(c, "upload-cdn")
+					return cli.NewExitError("Expected at least 2 parameters", 1)
+				}
+				fmt.Printf("src:%s\ndst:%s\nisCache:%v\n", c.Args()[0:len(c.Args())-1], c.Args()[len(c.Args())-1], c.Bool("cache"))
+				return nil
+			},
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		if _, ok := err.(*cli.ExitError); !ok {
+			// Exit errors are already printed
+			fmt.Println(err)
+		}
+	}
+}
+
+func readConfig(conf string) (map[string]interface{}, error) {
+	data, err := ioutil.ReadFile(conf)
+	if err != nil {
+		return nil, err
+	}
+	result := make(map[string]interface{})
+	err = yaml.Unmarshal(data, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func lsBranch(service string) error {
+	serviceConf, err := readConfig(fmt.Sprintf("%s/.dpcfg/service.yaml", os.Getenv("HOME")))
+	if err != nil {
+		return err
+	}
+	var gitUrl string
+	for _, v := range serviceConf {
+		vAssert, _ := v.(map[interface{}]interface{})
+		item, ok := vAssert[service]
+		if ok {
+			vItem, _ := item.(map[string]interface{})
+			gitUrl = fmt.Sprintf("%s", vItem["gitUrl"])
+			break
+		}
+	}
+	if "" == gitUrl {
+		return errors.New(fmt.Sprintf("Error: No such service: %s", service))
+	}
+	gitLsRemote := fmt.Sprintf("git ls-remote --head %s | awk -F '/' '{print $NF}'", gitUrl)
+	cmd := exec.Command("bash", "-c", gitLsRemote)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"ServiceName", "Branches"})
+	table.SetHeaderColor(tablewriter.Colors{tablewriter.FgGreenColor, tablewriter.Bold},
+		tablewriter.Colors{tablewriter.FgGreenColor, tablewriter.Bold})
+	table.SetAutoMergeCells(true)
+	var data [][]string
+	for _, branch := range strings.Fields(string(output)) {
+		data = append(data, []string{service, branch})
+	}
+	table.AppendBulk(data)
+	table.Render()
+	return nil
+}
+
+func lsService() error {
+	serviceConf, err := readConfig(fmt.Sprintf("%s/.dpcfg/service.yaml", os.Getenv("HOME")))
+	if err != nil {
+		return err
+	}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"ServiceType", "ServiceName"})
+	table.SetHeaderColor(tablewriter.Colors{tablewriter.FgGreenColor, tablewriter.Bold},
+		tablewriter.Colors{tablewriter.FgGreenColor, tablewriter.Bold})
+	table.SetAutoMergeCells(true)
+	var data [][]string
+	for k, v := range serviceConf {
+		vAssert, _ := v.(map[interface{}]interface{})
+		for svc, _ := range vAssert {
+			data = append(data, []string{k, fmt.Sprintf("%s", svc)})
+		}
+	}
+	table.AppendBulk(data)
+	table.Render()
+	return nil
+}
+
+func deployMicroService(service, branch, env string) error {
+	if env != "dev" && env != "test" && env != "stage" && env != "production" {
+		return errors.New(fmt.Sprintf("Error: No such env: %s, the available env is [dev, test, stage, production]", env))
+	}
+	serviceConf, err := readConfig(fmt.Sprintf("%s/.dpcfg/service.yaml", os.Getenv("HOME")))
+	if err != nil {
+		return err
+	}
+	conf, err := readConfig(fmt.Sprintf("%s/.dpcfg/conf.yaml", os.Getenv("HOME")))
+	if err != nil {
+		return err
+	}
+
+	var gitUrl string
+	var buildScriptPath string
+	for _, v := range serviceConf {
+		vAssert, _ := v.(map[interface{}]interface{})
+		item, ok := vAssert[service]
+		if ok {
+			vItem, _ := item.(map[string]interface{})
+			gitUrl = fmt.Sprintf("%s", vItem["gitUrl"])
+			buildScriptPath = fmt.Sprintf("%s", vItem["buildScriptPath"])
+			break
+		}
+	}
+	if "" == gitUrl || "" == buildScriptPath {
+		return errors.New(fmt.Sprintf("Error: No such service: %s", service))
+	}
+
+	fmt.Println("Step1: [ Pull Code ] *******************************************")
+	tmpDir, err := ioutil.TempDir("/tmp", "deploy")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tmpDir)
+
+	gitClone := fmt.Sprintf("git clone -b %s %s %s", branch, gitUrl, tmpDir)
+	err = RunCommand("bash", "-c", "-x", gitClone)
+	if _, ok := err.(*exec.ExitError); ok { // there is error code
+		return err
+	}
+
+	fmt.Println("Step2: [ Build Package ] ***************************************")
+	mvnBuild := fmt.Sprintf("cd %s/%s && sh build.sh", tmpDir, buildScriptPath)
+	fmt.Println(mvnBuild)
+	err = RunCommand("bash", "-c", "-x", mvnBuild)
+	if _, ok := err.(*exec.ExitError); ok {
+		return err
+	}
+
+	fmt.Println("Step3: [ Build Docker Image ] **********************************")
+	_, err = CopyFile(fmt.Sprintf("%s/%s/%s", tmpDir, buildScriptPath, conf["MicroServiceDockerfile"]),
+		fmt.Sprintf("%s/.dpcfg/%s", os.Getenv("HOME"), conf["MicroServiceDockerfile"]))
+	if err != nil {
+		return err
+	}
+
+	dockerImageName := fmt.Sprintf("%s/%s", conf["DockerRepo"], service)
+	timestamp := time.Now().UTC().Format("20060102-150405")
+	gitRevParse := fmt.Sprintf("cd %s && git rev-parse --short HEAD", tmpDir)
+	commitId, err := exec.Command("bash", "-c", gitRevParse).Output()
+	deployType := "cli"
+	dockerImageTag := fmt.Sprintf("%s-%s-%s-%s", timestamp, strings.TrimRight(string(commitId), "\n"), deployType, branch)
+	packgeName := parsePackageName(fmt.Sprintf("%s/%s/pom.xml", tmpDir, buildScriptPath))
+
+	dockerBuild := fmt.Sprintf("cd %s/%s && docker build --build-arg JAR_FILE=%s -t %s:%s .", tmpDir, buildScriptPath,
+		packgeName, dockerImageName, dockerImageTag)
+	fmt.Println(dockerBuild)
+	err = RunCommand("bash", "-c", "-x", dockerBuild)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Step4: [ Push Docker Image ] ***********************************")
+	dockerLogin := fmt.Sprintf("docker login -u %s -p `cat %s` %s", conf["DockerRepoUser"],
+		fmt.Sprintf("%s/.dpcfg/%s", os.Getenv("Home"), conf["DockerRepoPassFile"]), conf["DockerRepoUrl"])
+	err = RunCommand("bash", "-c", "-x", dockerLogin)
+	if err != nil {
+		return err
+	}
+	dockerPush := fmt.Sprintf("docker push %s:%s", dockerImageName, dockerImageTag)
+	err = RunCommand("bash", "-c", "-x", dockerPush)
+	if err != nil {
+		return err
+	}
+	defer RemoveDockerImage(fmt.Sprintf("%s:%s", dockerImageName, dockerImageTag))
+
+	fmt.Println("Step5: [ Deploy to Kubernetes ] ********************************")
+	helmRepoAdd := fmt.Sprintf("helm repo add deploy %s", conf["HelmRepo"])
+	err = RunCommand("bash", "-c", "-x", helmRepoAdd)
+	if err != nil {
+		return err
+	}
+	var appName string
+	if env == "dev" {
+		appName = fmt.Sprintf("%s-dev", service)
+	} else {
+		appName = service
+	}
+	helmInstall := fmt.Sprintf(`helm upgrade --wait --timeout 600 --install -f %s/default -f %s/values/%s/values-%s.yaml --set 'image.tag=%s,image.repository=%s' --kubeconfig %s --kube-context %s --namespace %s %s deploy/microservice`,
+		conf["HelmValuesPath"], conf["HelmValuesPath"], service, env, dockerImageTag, service, fmt.Sprintf("%s/.dpcfg/kube-config", os.Getenv("HOME")), conf[fmt.Sprintf("kubeCtx%s", env)], env, appName)
+	err = RunCommand("bash", "-c", "-x", helmInstall)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func CopyFile(dstName, srcName string) (written int64, err error) {
+	src, err := os.Open(srcName)
+	if err != nil {
+		return
+	}
+	defer src.Close()
+
+	dst, err := os.Create(dstName)
+	if err != nil {
+		return
+	}
+	defer dst.Close()
+
+	return io.Copy(dst, src)
+}
+
+type Project struct {
+	XMLName    xml.Name `xml:"project"`
+	ArtifactId string   `xml:"artifactId"`
+	Packaging  string   `xml:"packaging"`
+	Version    string   `xml:"version"`
+}
+
+//
+// 解析 pom.xml 获取构建 jar 包名称
+//
+func parsePackageName(pom string) string {
+	data, _ := ioutil.ReadFile(pom)
+	project := Project{}
+	xml.Unmarshal(data, &project)
+	return fmt.Sprintf("%s-%s.%s", project.ArtifactId, project.Version, project.Packaging)
+}
+
+func RunCommand(name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+
+	stdout, err := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+
+	if err != nil {
+		return err
+	}
+
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+
+	for {
+		tmp := make([]byte, 1024)
+		_, err := stdout.Read(tmp)
+		fmt.Println(string(tmp))
+		if err != nil {
+			break
+		}
+	}
+
+	if err = cmd.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func RemoveDockerImage(image string) error {
+	dockerRmi := fmt.Sprintf("docker rmi %s", image)
+	return RunCommand("bash", "-c", dockerRmi)
+}
